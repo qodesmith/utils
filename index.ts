f637719bc82e88f9ce5a56b35cb279a6419796f2
Qodesmith <theqodesmith@gmail.com>
2024-06-06 17:22:35 -0400

    Added getLocalDate and log

--- index.ts
+++ index.ts
@@ -1,1 +1,35 @@
-console.log("Hello via Bun!");
+export function getLocalDate(timeZone?: string) {
+  return new Date().toLocaleString('en-US', {timeZone})
+}
+
+export const log = (() => {
+  const ansi = {
+    success: '\x1b[32m%s\x1b[0m',
+    error: '\x1b[31m%s\x1b[0m',
+    warning: '\x1b[33m%s\x1b[0m',
+  }
+
+  function logger(type: keyof typeof ansi, messages: any[]) {
+    const ansiStr = ansi[type]
+    const msgs = [ansiStr, `[${getLocalDate()}]`].concat(
+      messages.flatMap(msg => [ansiStr, msg])
+    )
+
+    console.log(...msgs)
+  }
+
+  return {
+    text(...messages: any[]) {
+      console.log(`[${getLocalDate()}]`, ...messages)
+    },
+    success(...messages: any[]) {
+      logger('success', messages)
+    },
+    error(...messages: any[]) {
+      logger('error', messages)
+    },
+    warning(...messages: any[]) {
+      logger('warning', messages)
+    },
+  }
+})()
