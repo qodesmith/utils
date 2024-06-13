da97eeb8ee92077abcaf532d6a7d8d282d33d519
Qodesmith <theqodesmith@gmail.com>
2024-06-13 07:10:27 -0400

    Updated the log function. Removing the IIFE because bundlers can't exclude that from builds even if it's not imported.

--- index.ts
+++ index.ts
@@ -2,34 +2,35 @@ export function getLocalDate(timeZone?: string) {
   return new Date().toLocaleString('en-US', {timeZone})
 }
 
-export const log = (() => {
-  const ansi = {
-    success: '\x1b[32m%s\x1b[0m',
-    error: '\x1b[31m%s\x1b[0m',
-    warning: '\x1b[33m%s\x1b[0m',
-  }
+const ansi = {
+  success: '\x1b[32m%s\x1b[0m',
+  error: '\x1b[31m%s\x1b[0m',
+  warning: '\x1b[33m%s\x1b[0m',
+}
 
-  function logger(type: keyof typeof ansi, messages: any[]) {
-    const ansiStr = ansi[type]
-    const msgs = [ansiStr, `[${getLocalDate()}]`].concat(
-      messages.flatMap(msg => [ansiStr, msg])
-    )
+function logger(type: keyof typeof ansi, messages: any[]) {
+  const ansiStr = ansi[type]
+  const items = [ansiStr, `[${getLocalDate()}]`].concat(
+    messages.flatMap(item => {
+      const isObj = typeof item === 'object' || typeof item === 'function'
+      return isObj ? [item] : [ansiStr, item]
+    })
+  )
 
-    console.log(...msgs)
-  }
+  console.log(...items)
+}
 
-  return {
-    text(...messages: any[]) {
-      console.log(`[${getLocalDate()}]`, ...messages)
-    },
-    success(...messages: any[]) {
-      logger('success', messages)
-    },
-    error(...messages: any[]) {
-      logger('error', messages)
-    },
-    warning(...messages: any[]) {
-      logger('warning', messages)
-    },
-  }
-})()
+export const log = {
+  text(...items: any[]) {
+    console.log(`[${getLocalDate()}]`, ...items)
+  },
+  success(...items: any[]) {
+    logger('success', items)
+  },
+  error(...items: any[]) {
+    logger('error', items)
+  },
+  warning(...items: any[]) {
+    logger('warning', items)
+  },
+}
