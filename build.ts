85608de673b26085b71df8453750ced5b3eb4f38
Qodesmith <theqodesmith@gmail.com>
2024-06-13 09:44:36 -0400

    Dynamically creating index.ts

--- build.ts
+++ build.ts
@@ -1,6 +1,23 @@
+import fs from 'node:fs'
+import path from 'node:path'
 import dts from 'bun-plugin-dts'
 
 const start = performance.now()
+
+/**
+ * Get a list of all the modules in this project to dynamically construct an
+ * `index.ts` file so we don't "forget to add that last module we just created."
+ */
+const contents = fs.readdirSync('./src').reduce<string[]>((acc, item) => {
+  if (!item.endsWith('.ts')) return acc
+
+  const parsedItem = path.parse(item)
+  acc.push(`export * from './src/${parsedItem.name}'`)
+
+  return acc
+}, [])
+
+await Bun.write('./index.ts', `${contents.join('\n')}\n`)
 
 await Bun.build({
   entrypoints: ['./index.ts'],
