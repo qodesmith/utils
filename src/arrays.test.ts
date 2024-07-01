078abddc9b558ecdba7806d6d9a2ba4b0134519c
Qodesmith <theqodesmith@gmail.com>
2024-07-01 06:08:12 -0400

    chunkArray supports typed arrays

--- src/arrays.test.ts
+++ src/arrays.test.ts
@@ -6,4 +6,9 @@ test('chunkArray', () => {
   const newArr = chunkArray(arr, 2)
 
   expect(newArr).toEqual([[1, 2], [3, 4], [5, 6], [7]])
+
+  const float32Arr = new Float32Array([1, 2, 3, 4, 5, 6, 7])
+  const chunked32Arr = chunkArray(float32Arr, 3)
+
+  expect(chunked32Arr).toEqual([[1, 2, 3], [4, 5, 6], [7]])
 })
--- src/arrays.ts
+++ src/arrays.ts
@@ -1,13 +1,29 @@
 /**
- * Chunks an array.
+ * Chunks an array. TypedArray's (i.e. `Float32Array`, `Uint16Array`, etc.) will
+ * be chunked into regular arrays. Each chunk will contain the numeric values
+ * from the original typed array as standard numbers rather than in the typed
+ * array format.
  *
  * ```
  * const arr = [1, 2, 3, 4, 5, 6]
  * const newArr = chunkArray(arr, 2) // [[1, 2], [3, 4], [5, 6]]
  * ```
  */
-export function chunkArray<T>(arr: T[], size: number): T[][] {
-  return Array.from({length: Math.ceil(arr.length / size)}, (v, i) =>
-    arr.slice(i * size, i * size + size)
-  )
+export function chunkArray<T>(arr: ArrayLike<T>, size: number): T[][] {
+  const length = arr.length
+  const finalResults: T[][] = []
+  let tempArray: T[] = []
+
+  for (let i = 0; i < length; i++) {
+    tempArray.push(arr[i])
+
+    if (tempArray.length === size) {
+      finalResults.push(tempArray)
+      tempArray = []
+    } else if (i === length - 1) {
+      finalResults.push(tempArray)
+    }
+  }
+
+  return finalResults
 }
