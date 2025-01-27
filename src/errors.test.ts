3b82dae3c891ddeb6c546ba361ad47b8781b78ae
Qodesmith <theqodesmith@gmail.com>
2025-01-27 07:12:23 -0500

    Added prettyStack option to errorToObject

--- src/errors.test.ts
+++ src/errors.test.ts
@@ -1,7 +1,7 @@
-import {expect, test} from 'bun:test'
+import {expect, test, describe} from 'bun:test'
 import {errorToObject} from './errors'
 
-test('errorToObject', () => {
+describe('errorToObject', () => {
   const expectedProperties = [
     'column',
     'line',
@@ -11,30 +11,49 @@ test('errorToObject', () => {
     'stack',
   ]
 
-  const plainError = new Error('Nope!')
-  const plainErrorObj = errorToObject(plainError)
-  expect(plainErrorObj).toHaveProperty('message', 'Nope!')
-  expectedProperties.forEach(prop => {
-    expect(plainErrorObj).toHaveProperty(prop)
+  test('no options', () => {
+    const plainError = new Error('Nope!')
+    const plainErrorObj = errorToObject(plainError)
+    expect(plainErrorObj).toHaveProperty('message', 'Nope!')
+    expectedProperties.forEach(prop => {
+      expect(plainErrorObj).toHaveProperty(prop)
+    })
+
+    const errorWithCauseText = new Error('Fail!', {cause: 'text'})
+    const errorWithCauseTextObj = errorToObject(errorWithCauseText)
+    expect(errorWithCauseTextObj).toHaveProperty('message', 'Fail!')
+    expect(errorWithCauseTextObj).toHaveProperty('cause', 'text')
+    expectedProperties.forEach(prop => {
+      expect(errorWithCauseTextObj).toHaveProperty(prop)
+    })
+
+    const errorWithCauseError = new Error('Boom!', {cause: new Error('Cause!')})
+    const errorWithCauseErrorObj = errorToObject(errorWithCauseError)
+    // expect(errorWithCauseErrorObj).toEqual({})
+    expect(errorWithCauseErrorObj).toHaveProperty('message', 'Boom!')
+    expect(errorWithCauseErrorObj).toHaveProperty(
+      ['cause', 'message'],
+      'Cause!'
+    )
+    expectedProperties.forEach(prop => {
+      expect(errorWithCauseErrorObj).toHaveProperty(prop)
+    })
+    expectedProperties.forEach(prop => {
+      expect(errorWithCauseErrorObj.cause).toHaveProperty(prop)
+    })
   })
 
-  const errorWithCauseText = new Error('Fail!', {cause: 'text'})
-  const errorWithCauseTextObj = errorToObject(errorWithCauseText)
-  expect(errorWithCauseTextObj).toHaveProperty('message', 'Fail!')
-  expect(errorWithCauseTextObj).toHaveProperty('cause', 'text')
-  expectedProperties.forEach(prop => {
-    expect(errorWithCauseTextObj).toHaveProperty(prop)
-  })
+  test.only('prettyStack', () => {
+    const expectedPropertiesPrettyStack = expectedProperties
+      .filter(property => property !== 'stack')
+      .concat(['stack_0', 'stack_1'])
+    const plainError = new Error('Nope!')
+    const plainErrorObj = errorToObject(plainError, {prettyStack: true})
 
-  const errorWithCauseError = new Error('Boom!', {cause: new Error('Cause!')})
-  const errorWithCauseErrorObj = errorToObject(errorWithCauseError)
-  // expect(errorWithCauseErrorObj).toEqual({})
-  expect(errorWithCauseErrorObj).toHaveProperty('message', 'Boom!')
-  expect(errorWithCauseErrorObj).toHaveProperty(['cause', 'message'], 'Cause!')
-  expectedProperties.forEach(prop => {
-    expect(errorWithCauseErrorObj).toHaveProperty(prop)
-  })
-  expectedProperties.forEach(prop => {
-    expect(errorWithCauseErrorObj.cause).toHaveProperty(prop)
+    expectedPropertiesPrettyStack.forEach(prop => {
+      expect(plainErrorObj).toHaveProperty(prop)
+    })
+    expect(plainErrorObj.stack_0).toBe('Error: Nope!')
+    expect(plainErrorObj.stack_1).toBeString()
   })
 })
--- src/errors.ts
+++ src/errors.ts
@@ -6,13 +6,28 @@
  * @param {any} error - The Error object to convert.
  * @returns {Record<string, unknown>} A plain object representation of the Error.
  */
-export function errorToObject(error: any): Record<string, unknown> {
+export function errorToObject(
+  error: any,
+  options?: {prettyStack: boolean}
+): Record<string, unknown> {
   return Object.getOwnPropertyNames(error).reduce<Record<string, unknown>>(
     (acc, key) => {
       // The `cause` property may be an error object as well.
       if (error[key] instanceof Error) {
-        const errorObj = errorToObject(error[key])
+        const errorObj = errorToObject(error[key], options)
         acc[key] = errorObj
+      } else if (
+        key === 'stack' &&
+        typeof error[key] === 'string' &&
+        options?.prettyStack
+      ) {
+        const stack = error.stack as string
+        const stackLines = stack.split('\n')
+
+        stackLines.forEach((line, i) => {
+          acc[`stack_${i}`] = line
+        })
+        delete error.stack
       } else {
         acc[key] = error[key]
       }
